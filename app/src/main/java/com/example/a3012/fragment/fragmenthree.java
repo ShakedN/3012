42a54ba75ec1941e7d2d4332daa50f0ab1d97295
ShakedN <shnuttman@gmail.com>
2024-12-22 20:43:18 +0200

    ver3

--- app/src/main/java/com/example/a3012/fragment/fragmenthree.java
+++ app/src/main/java/com/example/a3012/fragment/fragmenthree.java
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.a3012.CustomeAdapter;
 import com.example.a3012.R;
@@ -18,31 +20,29 @@ import java.util.ArrayList;
 
 import moudles.GroceryItem;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragmenthree#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class fragmenthree extends Fragment {
 
     private ArrayList<GroceryItem> dataset;
     private RecyclerView recyclerView;
-    private LinearLayoutManager layoutManager;
-    private CustomeAdapter addapter;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CustomeAdapter adapter;
+    private SearchView searchView;
 
     public fragmenthree() {
         // Required empty public constructor
     }
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_EMAIL = "email";
 
-
+    private String mParam1;
+    private String mParam2;
+    public static fragmenthree newInstance(String email) {
+        fragmenthree fragment = new fragmenthree();
+        Bundle args = new Bundle();
+        args.putString(ARG_EMAIL, email); // Put the email in the Bundle
+        fragment.setArguments(args);      // Attach the Bundle to the fragment
+        return fragment;
+    }
     public static fragmenthree newInstance(String param1, String param2) {
         fragmenthree fragment = new fragmenthree();
         Bundle args = new Bundle();
@@ -52,19 +52,65 @@ public class fragmenthree extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmentthree, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragmentthree, container, false);
+
+        // Initialize UI components
+        recyclerView = view.findViewById(R.id.recyclerViewResult);
+        searchView = view.findViewById(R.id.searchView);
+        TextView clientName = view.findViewById(R.id.clientName);
+        clientName.setText(getArguments().getString(ARG_PARAM1));
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+        // Prepare dataset
+        dataset = new ArrayList<>();
+        for (int i = 0; i < Data.nameArray.length; i++) {
+            dataset.add(new GroceryItem(
+                    Data.nameArray[i],
+                    Data.descriptionArray[i],
+                    Data.drawableArray[i],
+                    Data.id_[i]
+            ));
+        }
+
+        // Set up RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new CustomeAdapter(dataset);
+        recyclerView.setAdapter(adapter);
+
+        // Set up SearchView
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.filter(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.filter(newText);
+                return true;
+            }
+        });
+        // Search Icon click functionality
+        // Search Icon click functionality
+        ImageView searchIcon = view.findViewById(R.id.searchIcon);
+
+        // Show the SearchView when search icon is clicked
+        searchIcon.setOnClickListener(v -> {
+            // Make the SearchView visible
+            searchView.setVisibility(View.VISIBLE);
+            searchView.requestFocus();
+        });
+
+        return view;
     }
 }
